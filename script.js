beb6d6ee863228b8415de983a394697931ecea9e
Hitesh <hiteshkumarnain@gmail.com>
2024-11-12 01:49:43 +0530

    just some mor changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const navbar = document.getElementById("navbar");
+    const navbar = document.getElementById("navbar");
 
-  // Add event listeners for mouseover and mouseout
+// Add event listeners for mouseover and mouseout
   navbar.addEventListener("mouseover", () => {
     navbar.style.height = "10vw";
   });
@@ -9,12 +9,12 @@ const navbar = document.getElementById("navbar");
     navbar.style.height = "2.75vw";
   });
 
-window.addEventListener("scroll", () => {
-    const container = document.querySelector('#navbar');
-    const stickyPoint = container.offsetTop;
-    const scrollPosition = window.scrollY;
+    window.addEventListener("scroll", () => {
+        const container = document.querySelector('#navbar');
+        const stickyPoint = container.offsetTop;
+        const scrollPosition = window.scrollY;
 
-    // Toggle position based on scroll
+// Toggle position based on scroll
     if (scrollPosition > stickyPoint) {
       navbar.style.position = 'fixed';
       navbar.style.top = '0';
